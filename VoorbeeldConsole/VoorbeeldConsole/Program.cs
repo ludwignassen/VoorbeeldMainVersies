ef2a9bc02fbce508aad43e4cbcd5bb7a0959a003
Ludwig Nassen <35228327+ludwignassen@users.noreply.github.com>
2023-01-19 09:56:53 +0100

    Hello World v1.0

---
+++ VoorbeeldConsole/VoorbeeldConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoorbeeldConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Druk op een toets om te stoppen...");
+            Console.ReadKey();
+        }
+    }
+}
